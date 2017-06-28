09d949e668b085f9cd36d887aea56bd3891f0ec8
declantyson <declantyson@hotmail.co.uk>
2017-06-28 16:43:37 +0100

    Add some requirements

--- tests/coberta.js
+++ tests/coberta.js
@@ -2,9 +2,47 @@
  *  Coberta / Tests / Application
  *  Declan Tyson
  *  v0.1.0
- *  27/06/2017
+ *  28/06/2017
  */
 
+const notYetImplemented = false;
+
 describe('Coberta', function() {
+
+    it('should probably have a fancy clock, because all dashboards have a fancy clock', function(){
+        assert(notYetImplemented, 'fancy clock not found');
+    });
+
+    it('should pull open issues assigned to me from my HWC tracker for a list of specified projects', function(){
+        assert(notYetImplemented, 'hwc issues not found');
+    });
+
+    it('should pull open stories assigned to me from my current project jira', function(){
+        assert(notYetImplemented, 'jira stories not found');
+    });
+
+    it('should pull my latest run data from fitbit and when I am next scheduled for exercise', function(){
+        assert(notYetImplemented, 'fitbit data not found');
+    });
+
+    it('should pull my nutritional statistics for the day from myfitnesspal and display them as a chart', function(){
+        assert(notYetImplemented, 'myfitnesspal data not found');
+    });
+
+    it('should pull the last paragraph from the latest modified blog from the 2017.declantyson repository and provide me with an option to extend it', function(){
+        assert(notYetImplemented, 'blog data not found');
+    });
+
+    it('should prompt me at a specified time on a specified day to write a paragraph on a specified blog topic and save it to a .md file in the 2017.declantyson repository', function(){
+        assert(notYetImplemented, 'blog prompt not found');
+    });
+
+    it('should pull the last paragraph of the last chapter I worked on in StoryStore and provide me with an option to extend it', function(){
+        assert(notYetImplemented, 'book data not found');
+    });
+
+    it('should prompt me at a specified time on a specified day to write a paragraph on the last chapter I worked on in StoryStore', function(){
+        assert(notYetImplemented, 'book prompt not found');
+    });
 
 });
